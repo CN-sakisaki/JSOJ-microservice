d2aaa5c83dd9cb7af215a5f5c567f6fe1837a157
sakisaki <189444236@qq.com>
2025-02-24 08:42:11 +0800

    feat：token验证测试

--- jsoj-backend-microservice/jsoj-backend-gateway/src/test/java/com/js/jsojbackendgateway/JsojBackendGatewayApplicationTests.java
+++ jsoj-backend-microservice/jsoj-backend-gateway/src/test/java/com/js/jsojbackendgateway/JsojBackendGatewayApplicationTests.java
@@ -10,4 +10,5 @@ class JsojBackendGatewayApplicationTests {
     void contextLoads() {
     }
 
+
 }
--- jsoj-backend-microservice/jsoj-backend-user-service/src/test/java/com/js/jsojbackenduserservice/JsojBackendUserServiceApplicationTests.java
+++ jsoj-backend-microservice/jsoj-backend-user-service/src/test/java/com/js/jsojbackenduserservice/JsojBackendUserServiceApplicationTests.java
@@ -1,7 +1,12 @@
 package com.js.jsojbackenduserservice;
 
+import com.js.jsojbackendcommon.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
 class JsojBackendUserServiceApplicationTests {
@@ -10,4 +15,12 @@ class JsojBackendUserServiceApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testJwtGenerationAndValidation() {
+        String token = JwtUtils.generateAccessToken(1L);
+        System.out.println(token);
+        assertTrue(JwtUtils.validateToken(token));
+        Claims claims = JwtUtils.parseToken(token);
+        System.out.println(claims.get("userId"));
+    }
 }
