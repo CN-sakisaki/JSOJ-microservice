cd0258c1f0356ba38b2f10cd0016accfbea55953
sakisaki <189444236@qq.com>
2025-03-01 18:34:35 +0800

    style：调整代码样式

--- jsoj-backend-microservice/jsoj-backend-user-service/src/main/java/com/js/jsojbackenduserservice/config/OpenApiConfig.java
+++ jsoj-backend-microservice/jsoj-backend-user-service/src/main/java/com/js/jsojbackenduserservice/config/OpenApiConfig.java
@@ -1,24 +1,32 @@
 package com.js.jsojbackenduserservice.config;
 
-// /**
-//  * openAPI文档配置
-//  * @author sakisaki
-//  * @date 2025/2/28 17:25
-//  */
-// @Configuration
-// public class OpenApiConfig {
-//
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         Server devServer = new Server();
-//         devServer.setUrl("http://localhost:8102/api/user/v3/api-docs");
-//         devServer.setDescription("开发环境服务器");
-//
-//         return new OpenAPI()
-//                 .info(new Info()
-//                         .title("API 文档")
-//                         .version("1.0")
-//                         .description("接口说明"))
-//                 .servers(Collections.singletonList(devServer));
-//     }
-// }
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+/**
+ * openAPI文档配置
+ * @author sakisaki
+ * @date 2025/2/28 17:25
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8102/api/user/v3/api-docs");
+        devServer.setDescription("开发环境服务器");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API 文档")
+                        .version("1.0")
+                        .description("接口说明"))
+                .servers(Collections.singletonList(devServer));
+    }
+}
